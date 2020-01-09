05f01301aa046e6189cc48473f972ce7d0acad90
Saeed-AL1 <saym544@gmail.com>
2020-01-09 01:02:56 -0500

    Removed some codes

--- script.py
+++ script.py
@@ -1,23 +1,24 @@
-import math
-import sys
-from os import rename
+# import math
+# import sys
+# from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
+# def greet(who_to_greet):
 
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
-r = requests.get("https://coreyms.com")
-print(r.status_code)
-print(r.status_code)
-print(greet('World'))
-print(greet('Saeed'))
-# name = input('Your Name? ')
-# print("Hello,", name)
+ r = requests.get("https://coreyms.com")
+ print(r.status_code)
+ print(r.ok)
+# print(r.status_code)
+# print(greet('World'))
+# print(greet('Saeed'))
+# # name = input('Your Name? ')
+# # print("Hello,", name)
